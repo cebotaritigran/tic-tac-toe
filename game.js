2cccd83074ea7093db62b97326c1391b07c9e11a
CebotariTigran <cebotaritigran@gmail.com>
2023-03-09 15:22:17 +0100

    Add turn function

--- game.js
+++ game.js
@@ -12,29 +12,47 @@ const Players = (player, sign) => {
 }
 
 //asigning 2 players
-const playerfirst = Players('hehe', "x");
-const playersecond = Players('hehe', "o");
 
-// game factorial function 
-const Game = (grid, playerOne, playerTwo) => {
-    let playerTurn = true;
-    // put x or o on display
-    const turn = () => {
-        if (playerTurn) {
-            grid.textContent = playerOne.sign;
-            playerTurn = false
-        } else if (playerTurn == false) {
-            grid.textContent = playerTwo.sign;
-            playerTurn = true
+const gameController = (() => {
+    const playerfirst = Players('hehe', "x");
+    const playersecond = Players('hehe', "o");
+    let playerTurn = 0;
+    // game factory function 
+    const Game = (grid, playerOne, playerTwo) => {
+        const winningCombos = () => {
+            const combos = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 4, 8],
+                [2, 4, 6],
+            ];
         }
-    }
-    return { turn }
-}
+        // put x or o on display
+        const turn = () => {
+            if (grid.textContent === "") {
+                if (playerTurn % 2 == 0) {
+                    grid.textContent = playerOne.sign;
+                    playerTurn++;
+                    console.log(playerTurn)
+                } else if (playerTurn % 2 != 0) {
+                    grid.textContent = playerTwo.sign;
+                    playerTurn++;
+                }
+            }
 
-// game board display
-const gridBox = document.querySelectorAll('.grid-box');
-for (let i = 0; i < gridBox.length; i++) {
-    gridBox[i].textContent = gameBoard.board[i];
-    game = Game(gridBox[i], playerfirst, playersecond)
-    gridBox[i].addEventListener('click', game.turn)
-}
+        }
+        return { turn }
+    }
+
+    // game board display
+    const gridBox = document.querySelectorAll('.grid-box');
+    for (let i = 0; i < gridBox.length; i++) {
+        gridBox[i].textContent = gameBoard.board[i];
+        game = Game(gridBox[i], playerfirst, playersecond)
+        gridBox[i].addEventListener('click', game.turn)
+    }
+})()
